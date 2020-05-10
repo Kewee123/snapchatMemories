227c336e9a37ff21005a4dbf47232d6ce82d74ad
Maung Naing <mpnaing@ucanr.edu>
2020-05-09 21:44:20 -0700

    Needed to order the struct that it returns but guess it isn't working

--- convert.py
+++ convert.py
@@ -10,7 +10,6 @@ dir_path = dir_path + "\\convertedMemories.json"
 with open(dir_path, 'r') as f:
     data = json.load(f)
 
-
 """ rawLinks={}
 rawType=[]
 
@@ -22,7 +21,9 @@ print(rawType) """
 
 def geturl(rawlink: str, date: str, dataType: str):
     r = requests.post(rawlink)
-    return {date, r.text, dataType}
+    returnDict = {}
+    returnDict[0] = {date, r.text, dataType}
+    return list(returnDict.values())
 
 links = []
 with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
@@ -39,7 +40,13 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
             print(f"Exception when processing result {futures[future]}")
 
 """ print(links) """
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
+for i in links:
+    print(i)
 with open('aws_links.json', 'w') as f:
-    json.dump({
-        'links': links
-    }, f)
+    json.dump(links, f, default=set_default)
