a1fdc8065c6482161bfc37f6461ec4892260e0fd
Maung Naing <mpnaing@ucanr.edu>
2020-05-10 00:00:17 -0700

    Named downloaded files by date

--- downloadFiles.py
+++ downloadFiles.py
@@ -6,14 +6,23 @@ import urllib.request
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 
-pathToFile = dir_path + "\\awslinks.json"
+pathToFile = dir_path + "\\aws_links.json"
 print(pathToFile)
 
 with open(pathToFile, 'r') as infile:
     links = json.load(infile)
-    for i in links["links"]:  
-        name = i[-8:]
-        urllib.request.urlretrieve(i, name)
+    for i in links:  
+        date = i["date"]
+        dataType = i["type"]
+        link = i["link"]
+        
+        name = date.replace(":", "")
+        if(dataType == "PHOTO"):
+            name+=".jpg"
+        else:
+            name+=".mp4"
+
+        urllib.request.urlretrieve(link, name)
 
 
        
